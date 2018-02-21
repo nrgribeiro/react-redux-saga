395284704bdfc8f3f9f3cf084ec0e6e82c47eff0
Valentino Gagliardi <valentino@valentinog.com>
2018-02-21 11:13:55 +0100

    Added: webpack 4, transpiling Javascript ES6 with Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log(`I'm a silly antry point`);
+console.log(`I'm a silly entry point`);
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
