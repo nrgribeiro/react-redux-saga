5266a7dca0c855239bd9adacda9ba273bf77d02d
Nuno Ribeiro <nribeiro@gmail.com>
2018-09-19 11:26:39 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-console.log(`I'm a silly entry point`);
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
-import App from "./App";
 import style from "./main.css";
+import './js/index.js';
